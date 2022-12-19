9cf82043c8746d23d578b0ffbc7be0dd10be7be7
uttsal timur <uttsal.ts@phystech.edu>
2022-12-19 23:04:24 +0300

    Deleted files to upload all at once

--- src/main/java/homework/java/service/dao/AbstractDao.java
+++
@@ -1,7 +0,0 @@
-package homework.java.service.dao;
-
-import java.sql.SQLException;
-
-public abstract class AbstractDao<T> {
-    public abstract void save(Iterable<T> smth) throws SQLException;
-}
